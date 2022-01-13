83711452e226e91957b7538e2b668c9934044ace
KJI-DEC <kji_dec22@ewhain.net>
2022-01-13 20:02:18 +0900

    Update part4. Script

---
+++ Ball/Assets/Ball.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // float startingPoint;
+    // SphereCollider myCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // myCollider = GetComponent<SphereCollider>();
+        // Rigidbody myRigidbody = GetComponent<Rigidbody>();
+        // Debug.Log("UseGravity?: " + myRigidbody.useGravity);
+        // Debug.Log("Start");
+        // startingPoint = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // myCollider.radius += 0.01f;
+        // float distance;
+        // distance = transform.position.z - startingPoint;
+        // Debug.Log(distance);
+    }
+}
