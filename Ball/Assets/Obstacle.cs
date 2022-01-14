17c54b69eb2c52ac70251108604e471a2aef500a
KJI-DEC <kji_dec22@ewhain.net>
2022-01-14 22:26:01 +0900

    Update part7. 아이템과 장애물

--- Ball/Assets/Obstacle.cs
+++ Ball/Assets/Obstacle.cs
@@ -6,14 +6,25 @@ public class Obstacle : MonoBehaviour
 {
     float delta = -0.1f;
     // Start is called before the first frame update
+    // void TestMethod(string name){
+    //     float distance = Vector3.Distance(GameObject.Find(name).transform.position, transform.position);
+    //     Debug.Log(name + "까지 거리: " + distance);
+    // }
+    void OnCollisionEnter(Collision collision) {
+        // Debug.Log(collision.gameObject.name);
+        Vector3 direction = transform.position - collision.gameObject.transform.position;
+        direction = direction.normalized * 1000;
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(direction);
+    }
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // TestMethod("Ball");
+
         float newXPosition = transform.localPosition.x + delta;
         transform.localPosition = new Vector3(newXPosition, transform.localPosition.y, transform.localPosition.z);
         if(transform.localPosition.x < -3.5){
