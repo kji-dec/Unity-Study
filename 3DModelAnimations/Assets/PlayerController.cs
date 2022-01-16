d817cb3d650c4df1f8f1e6ec97f79e22fc4bd8cb
KJI-DEC <kji_dec22@ewhain.net>
2022-01-16 14:27:33 +0900

    Update 3DModel/Animations

---
+++ 3DModelAnimations/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I)){
+            // animator.Play("Idle");
+            animator.SetFloat("moveSpeed", 0.0f);
+        }
+        else if(Input.GetKeyDown(KeyCode.R)){
+            // animator.Play("RUN00_F");
+            animator.SetFloat("moveSpeed", 5.0f);
+        }
+    }
+}
